6271723e22c75a35a5bfb8c5f2f9b822bc77af00
kguinto <klogold1@gmail.com>
2018-05-16 15:02:38 -0700

    Implemented reducer to handle board initialization and cell revealing

---
+++ src/reducers/reducers.js
@@ -0,0 +1,32 @@
+import { combineReducers } from 'redux';
+import { INITIALIZE_BOARD, REVEAL_CELL } from '../actions/actions';
+import {
+  createBlankBoard,
+  placeMines,
+  updateNumbers,
+  revealCell,
+} from './boardFunctions';
+
+// const printifyBoard = state => state.map(row => row.map(cell => `${cell.value}${(cell.visible) ? '!' : ''}`));
+
+function board(state = [], action) {
+  switch (action.type) {
+    case INITIALIZE_BOARD:
+      state = createBlankBoard(action.rows, action.columns);
+      var { state, mines } = placeMines(state, action.mines);
+      state = updateNumbers(state, mines);
+      return state;
+
+    case REVEAL_CELL:
+      state = JSON.parse(JSON.stringify(state)); // deep copy
+      state = revealCell(state, action.row, action.column);
+      return state;
+
+    default:
+      return state;
+  }
+}
+
+export default combineReducers({
+  board,
+});
