58147f26bd6af8f3cd20d7748a7d967f12201cbc
kguinto <klogold1@gmail.com>
2018-05-16 14:56:38 -0700

    Implemented Cell to render based on visibility in board state and handle left and right clicks

---
+++ src/components/Cell.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+// disable context menu for right clicks
+document.oncontextmenu = function() {
+  return false;
+};
+
+class Cell extends Component {
+  componentDidUpdate() {
+    if (
+      this.props.playing &&
+      this.props.cell.visible &&
+      this.props.cell.value === 'M'
+    ) {
+      this.props.endGame(false);
+    }
+  }
+
+  render() {
+    let { row, column, cell, revealCell, playing } = this.props;
+
+    return (
+      <div
+        className={`cell ${cell.visible ? 'shown-cell' : 'hidden-cell'}`}
+        onClick={() => {
+          if (playing && !cell.visible) revealCell(row, column);
+        }}
+        onContextMenu={() => {
+          console.log(`R ${row}, ${column}`);
+        }}
+      >
+        {cell.visible && cell.value ? cell.value : ''}
+      </div>
+    );
+  }
+}
+
+export default Cell;
