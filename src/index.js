82f2f4370c5e7cf714a52ed6036b499090500495
kguinto <klogold1@gmail.com>
2018-05-16 14:49:05 -0700

    Initialized app entry with redux

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/reducers';
+import './index.css';
+import App from './containers/App';
+import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(rootReducer);
+
+store.subscribe(() => {
+  store.getState();
+});
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+registerServiceWorker();
