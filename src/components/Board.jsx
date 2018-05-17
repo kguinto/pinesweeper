8ff645942ef5f7ba1f07f86e9756e6cbb80d706b
kguinto <klogold1@gmail.com>
2018-05-16 20:59:50 -0700

    Moved end game condition checking from Cell to Board and added checking for win condition

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,27 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Cell from '../containers/Cell';
 import './Board.css';
 
-const Board = ({ board, playing, endGame }) => {
-  return (
-    <div className="board">
-      {board &&
-        board.map((rowItems, row) => (
-          <div className="board-row" key={`row_${row}`}>
-            {rowItems.map((cell, column) => (
-              <Cell
-                row={row}
-                column={column}
-                cell={cell}
-                playing={playing}
-                endGame={endGame}
-                key={`cell_${row}_${column}`}
-              />
-            ))}
-          </div>
-        ))}
-    </div>
-  );
-};
+class Board extends Component {
+  componentDidUpdate() {
+    // Lose conditions: mine is visible
+    // Win conditions: all mines are flagged or all non-mines are visible
+    let allNonMinesVisible = true;
+    let mineVisible = false;
+
+    if (this.props.playing) {
+      this.props.board.forEach(row => {
+        row.forEach(cell => {
+          if (cell.visible && cell.value === 'M') {
+            mineVisible = true;
+          }
+
+          if (!cell.visible && cell.value !== 'M') {
+            allNonMinesVisible = false;
+          }
+        });
+      });
+
+      if (mineVisible) {
+        this.props.endGame(false);
+      } else if (allNonMinesVisible) {
+        this.props.endGame(true);
+      }
+    }
+  }
+
+  render() {
+    let { board, playing, endGame } = this.props;
+
+    return (
+      <div className="board">
+        {board &&
+          board.map((rowItems, row) => (
+            <div className="board-row" key={`row_${row}`}>
+              {rowItems.map((cell, column) => (
+                <Cell
+                  row={row}
+                  column={column}
+                  cell={cell}
+                  playing={playing}
+                  endGame={endGame}
+                  key={`cell_${row}_${column}`}
+                />
+              ))}
+            </div>
+          ))}
+      </div>
+    );
+  }
+}
 
 export default Board;
