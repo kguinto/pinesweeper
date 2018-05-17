512e7a8bd325d452998fa9c007e1267feb974648
kguinto <klogold1@gmail.com>
2018-05-16 21:00:42 -0700

    Moved end game condition checking out of Cell to Board

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Cell.css';
 
 // disable context menu for right clicks
@@ -6,34 +6,18 @@ document.oncontextmenu = function() {
   return false;
 };
 
-class Cell extends Component {
-  componentDidUpdate() {
-    if (
-      this.props.playing &&
-      this.props.cell.visible &&
-      this.props.cell.value === 'M'
-    ) {
-      this.props.endGame(false);
-    }
-  }
-
-  render() {
-    let { row, column, cell, revealCell, playing } = this.props;
-
-    return (
-      <div
-        className={`cell ${cell.visible ? 'shown-cell' : 'hidden-cell'}`}
-        onClick={() => {
-          if (playing && !cell.visible) revealCell(row, column);
-        }}
-        onContextMenu={() => {
-          console.log(`R ${row}, ${column}`);
-        }}
-      >
-        {cell.visible && cell.value ? cell.value : ''}
-      </div>
-    );
-  }
-}
+const Cell = ({ row, column, cell, revealCell, playing }) => (
+  <div
+    className={`cell ${cell.visible ? 'shown-cell' : 'hidden-cell'}`}
+    onClick={() => {
+      if (playing && !cell.visible) revealCell(row, column);
+    }}
+    onContextMenu={() => {
+      console.log(`R ${row}, ${column}`);
+    }}
+  >
+    {cell.visible && cell.value ? cell.value : ''}
+  </div>
+);
 
 export default Cell;
