965a4531804b8fe9ae0a6dcbec5f007d2a9d8b2e
kguinto <klogold1@gmail.com>
2018-05-19 09:46:46 -0700

    Added action for flagging a cell

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,6 +1,6 @@
-export const INITIALIZE_BOARD = "INITIALIZE_BOARD";
-
-export const REVEAL_CELL = "REVEAL_CELL";
+export const INITIALIZE_BOARD = 'INITIALIZE_BOARD';
+export const REVEAL_CELL = 'REVEAL_CELL';
+export const FLAG_CELL = 'FLAG_CELL';
 
 export function initializeBoard(rows, columns, mines) {
   return { type: INITIALIZE_BOARD, rows, columns, mines };
@@ -8,4 +8,8 @@ export function initializeBoard(rows, columns, mines) {
 
 export function revealCell(row, column) {
   return { type: REVEAL_CELL, row, column };
+}
+
+export function flagCell(row, column) {
+  return { type: FLAG_CELL, row, column };
 }
