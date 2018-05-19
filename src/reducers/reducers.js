b3ecee9c5dc66333e010d5542cb725e1c5a21e81
kguinto <klogold1@gmail.com>
2018-05-19 09:49:18 -0700

    Added case for flagging a cell

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,10 +1,11 @@
 import { combineReducers } from 'redux';
-import { INITIALIZE_BOARD, REVEAL_CELL } from '../actions/actions';
+import { INITIALIZE_BOARD, REVEAL_CELL, FLAG_CELL } from '../actions/actions';
 import {
   createBlankBoard,
   placeMines,
   updateNumbers,
   revealCell,
+  flagCell,
 } from './boardFunctions';
 
 // const printifyBoard = state => state.map(row => row.map(cell => `${cell.value}${(cell.visible) ? '!' : ''}`));
@@ -20,6 +21,11 @@ function board(state = [], action) {
     case REVEAL_CELL:
       state = JSON.parse(JSON.stringify(state)); // deep copy
       state = revealCell(state, action.row, action.column);
+      return state;
+
+    case FLAG_CELL:
+      state = JSON.parse(JSON.stringify(state)); // deep copy
+      state = flagCell(state, action.row, action.column);
       return state;
 
     default:
