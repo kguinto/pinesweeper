7940fdb265e7ac2327c34a9f7a24c21c8196f0bf
kguinto <klogold1@gmail.com>
2018-05-19 09:48:55 -0700

    Added flagCell to props

--- src/containers/Cell.js
+++ src/containers/Cell.js
@@ -1,9 +1,10 @@
 import { connect } from 'react-redux';
-import { revealCell } from '../actions/actions';
+import { revealCell, flagCell } from '../actions/actions';
 import Cell from '../components/Cell';
 
 const mapDispatchToProps = dispatch => ({
   revealCell: (row, column) => dispatch(revealCell(row, column)),
+  flagCell: (row, column) => dispatch(flagCell(row, column)),
 });
 
 export default connect(() => ({}), mapDispatchToProps)(Cell);
