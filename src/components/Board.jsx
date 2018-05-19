3ae2af497308f40e4f0b3cbdc3e631aad8ab4227
kguinto <klogold1@gmail.com>
2018-05-19 14:36:33 -0700

    Added logic to tell App.jsx the number of mines, flags, and flagged mines, and updated to handle win condition of all mines flagged

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -8,10 +8,24 @@ class Board extends Component {
     // Win conditions: all mines are flagged or all non-mines are visible
     let allNonMinesVisible = true;
     let mineVisible = false;
+    let mines = 0;
+    let flags = 0;
+    let flaggedMines = 0;
 
-    if (this.props.playing) {
+    if (this.props.gameStatus === 'PLAYING') {
       this.props.board.forEach(row => {
         row.forEach(cell => {
+          if (cell.value === 'M') {
+            mines += 1;
+          }
+          if (cell.flag) {
+            flags += 1;
+          }
+
+          if (cell.flag && cell.value === 'M') {
+            flaggedMines += 1;
+          }
+
           if (cell.visible && cell.value === 'M') {
             mineVisible = true;
           }
@@ -22,16 +36,20 @@ class Board extends Component {
         });
       });
 
+      this.props.updateFlags(mines - flags);
+
       if (mineVisible) {
         this.props.endGame(false);
       } else if (allNonMinesVisible) {
+        this.props.endGame(true);
+      } else if (mines === flaggedMines) {
         this.props.endGame(true);
       }
     }
   }
 
   render() {
-    let { board, playing, endGame } = this.props;
+    let { board, gameStatus, endGame, startGame } = this.props;
 
     return (
       <div className="board">
@@ -43,8 +61,9 @@ class Board extends Component {
                   row={row}
                   column={column}
                   cell={cell}
-                  playing={playing}
+                  gameStatus={gameStatus}
                   endGame={endGame}
+                  startGame={startGame}
                   key={`cell_${row}_${column}`}
                 />
               ))}
