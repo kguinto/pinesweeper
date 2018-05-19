296a0ee902cbe1a93138c516ec0d39189f85d9e4
kguinto <klogold1@gmail.com>
2018-05-19 14:33:48 -0700

    Modified logic to decide what to show inside cell, and now shows emoji flags and bombs, and refactored `playing` prop to `gameStatus`

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -6,19 +6,51 @@ document.oncontextmenu = function() {
   return false;
 };
 
-const Cell = ({ row, column, cell, revealCell, flagCell, playing }) => (
-  <div
-    className={`cell ${cell.visible ? 'shown-cell' : 'hidden-cell'}`}
-    onClick={() => {
-      if (playing && !cell.visible) revealCell(row, column);
-    }}
-    onContextMenu={() => {
-      if (playing && !cell.visible) flagCell(row, column);
-    }}
-  >
-    {cell.visible && cell.value ? cell.value : ''}
-    {!cell.visible && cell.flag ? 'F' : ''}
-  </div>
-);
+const Cell = ({
+  row,
+  column,
+  cell,
+  revealCell,
+  flagCell,
+  gameStatus,
+  startGame,
+}) => {
+  let value = '';
+  if (
+    (cell.visible || (gameStatus !== 'PLAYING' && gameStatus !== 'INIT')) &&
+    !cell.flag &&
+    cell.value === 'M'
+  ) {
+    value = '💣';
+  } else if (cell.visible && cell.value !== 0) {
+    value = cell.value;
+  } else if (cell.flag && (gameStatus === 'PLAYING' || cell.value === 'M')) {
+    value = '🚩';
+  } else if (cell.flag && !gameStatus === 'PLAYING' && cell.value !== 'M') {
+    value = '!💣';
+  }
+
+  return (
+    <div
+      className={`cell ${cell.visible ? 'shown-cell' : 'hidden-cell'}`}
+      onClick={() => {
+        if (gameStatus === 'INIT') {
+          startGame();
+        }
+        if (
+          (gameStatus === 'INIT' || gameStatus === 'PLAYING') &&
+          !cell.visible &&
+          !cell.flag
+        )
+          revealCell(row, column);
+      }}
+      onContextMenu={() => {
+        if (gameStatus === 'PLAYING' && !cell.visible) flagCell(row, column);
+      }}
+    >
+      {value}
+    </div>
+  );
+};
 
 export default Cell;
