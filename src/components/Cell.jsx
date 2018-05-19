669810d60b917c203aec7e795b1a36ae0b157820
kguinto <klogold1@gmail.com>
2018-05-19 09:47:35 -0700

    Added flagCell method to props

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -6,17 +6,18 @@ document.oncontextmenu = function() {
   return false;
 };
 
-const Cell = ({ row, column, cell, revealCell, playing }) => (
+const Cell = ({ row, column, cell, revealCell, flagCell, playing }) => (
   <div
     className={`cell ${cell.visible ? 'shown-cell' : 'hidden-cell'}`}
     onClick={() => {
       if (playing && !cell.visible) revealCell(row, column);
     }}
     onContextMenu={() => {
-      console.log(`R ${row}, ${column}`);
+      if (playing && !cell.visible) flagCell(row, column);
     }}
   >
     {cell.visible && cell.value ? cell.value : ''}
+    {!cell.visible && cell.flag ? 'F' : ''}
   </div>
 );
 
